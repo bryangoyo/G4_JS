6c2871f9626aef5697c5b50c5638e0fc4812d08a
Bryan <bryangoyo@outlook.com>
2023-10-28 16:55:11 +0200

    form easteregg

--- Practica_JS/assets/js/countries.js
+++ Practica_JS/assets/js/countries.js
@@ -19,48 +19,49 @@ async function getCountries() {
     let countries = await results.json();
     console.log(countries.data)
 
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * max);
-    }    
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 let randomNumber;
 let randomcountry ;
-let hola = [];
+let listaTresPaisesCapital = [];
 
 // FOR PARA HACER UN PUSH A UN ARRAY DE TRES PAISES CON SUS CAPITAL GENERADOS ALEATORIAMENTE
 for (let x = 0; x < 3; x++){
-    let iiii = Math.floor(Math.random() * countries.data.countries.length);
-    let hhhh = countries.data.countries[iiii]; // PAIS/CAPITAL ---- {NAME: HOLANDA, CAPITAL: HOLANDA}
-    hola.push(hhhh);
+    let randomNumeroListaPaises = Math.floor(Math.random() * countries.data.countries.length);
+    let Pais_Capital = countries.data.countries[randomNumeroListaPaises]; // PAIS/CAPITAL ---- {NAME: HOLANDA, CAPITAL: HOLANDA}
+    listaTresPaisesCapital.push(Pais_Capital);
 }
     let valorArray = getRandomInt(3); // 0
     
-    let choosenPais = hola[valorArray].name; //ESPAÑA
-    let choosenCapital = hola[valorArray].capital; //MADRID
+    let choosenPais = listaTresPaisesCapital[valorArray].name; //ESPAÑA
+    let choosenCapital = listaTresPaisesCapital[valorArray].capital; //MADRID
 
   //Pais y Capital Respuesta Correcta
-    console.log(hola[valorArray].name);
-    console.log(hola[valorArray].capital);
+    console.log(listaTresPaisesCapital[valorArray].name);
+    console.log(listaTresPaisesCapital[valorArray].capital);
 
 document.getElementById("eastereggQuestion").innerHTML = "What is the capital of "+ "<strong>" + choosenPais + "</strong>";
 
 // FOR PARA CREAR LOS INPUTS CON LAS CAPITALES DEL ARRAY
   
-for (let x = 0; x < hola.length; x++){
-    console.log(hola[x].name);
+for (let x = 0; x < listaTresPaisesCapital.length; x++){
+    console.log(listaTresPaisesCapital[x].name);
 
-    let varvar = document.createElement("input");
-    varvar.type = "radio";
-    varvar.value = hola[x].capital;
+    let inputEGGCapital = document.createElement("input");
+    inputEGGCapital.type = "radio";
+    inputEGGCapital.value = listaTresPaisesCapital[x].capital;
+    inputEGGCapital.name = "capital";
 
-    let label = document.createElement('label')
-    label.innerText = hola[x].capital; 
+    let labelEGGCapital = document.createElement('label')
+    labelEGGCapital.innerText = listaTresPaisesCapital[x].capital; 
 
     let container = document.getElementById('eastereggRespuestas');
-    container.appendChild(varvar);
-    container.appendChild(label);
+    container.appendChild(inputEGGCapital);
+    container.appendChild(labelEGGCapital);
 }
-console.log(hola);
+console.log(listaTresPaisesCapital);
 
 // TENGO EL PAIS Y LA CAPITAL SELECCINADA Y COMPRARLA CON LA SELECCION DEL INPUT SELECCIONADO  EVENTO
 
