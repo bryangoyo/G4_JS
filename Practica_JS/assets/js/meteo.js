8100107d9ef5021e019eb29925b72e6575db504a
Jaume <136893910+Txaumep@users.noreply.github.com>
2023-10-29 23:58:19 +0100

    Update meteo.js
    minor updates with style, icon weather and button

--- Practica_JS/assets/js/meteo.js
+++ Practica_JS/assets/js/meteo.js
@@ -1,23 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     const apiKey = '13fd320ea826dcf519670ed2757e0cf6';
-    // const city = 'Madrid,es'; // 
+    // const city = 'Barcelona,es'; // 
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=13fd320ea826dcf519670ed2757e0cf6&units=metric&lang=es`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=13fd320ea826dcf519670ed2757e0cf6&units=metric&lang=en`)
     .then(response => response.json())
     .then(data => {
         const temperature = data.main.temp;
         const humidity = data.main.humidity;
         const description = data.weather[0].description;
-        // const imageMeteo = data.weather[0].main;
+        const wIcon = data.weather[0].icon;
+        
         const weatherInfoDiv = document.getElementById('weather-info');
-
+        
+        
         weatherInfoDiv.innerHTML = `
-            <div class="meteo">
-            <h1>Condiciones meteorológicas en Barcelona</h1>
-            <p>Temperatura: ${temperature}°C</p>
-            <p>Humedad: ${humidity}%</p>
-            <p>Cielo: ${description}</p>
-            <button type="button" text-align: right>Hide</button>
+            <div class="meteo" style= "text-align:center;">
+            <h2>Barcelona</h2>
+            <p>Temperature: ${temperature}°C</p>
+            <p>Humidity: ${humidity}%</p>
+            <p>Sky: ${description}</p>
+            <img src="assets/img/icons/${wIcon}.png">
+            <button type="button" style="text-align:right;">Hide</button>
             </div>
         `;
     })
